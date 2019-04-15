28d97419c459428392fc249d5a131086fb380b71
Charles.Swires <Charles.Swires@VIZHULL036.vizion.local>
2019-04-15 12:06:55 +0100

    Camel case

--- src/main/java/com/charlie/Controller.java
+++ src/main/java/com/charlie/Controller.java
@@ -17,63 +17,63 @@ public class Controller {
 	@Autowired
 	private MyService serv;
 	//@PreAuthorize("hasRole('IT_DEVELOPER')")
-	@RequestMapping(value = "/get-all", method=RequestMethod.GET)
+	@RequestMapping(value = "/gelAll", method=RequestMethod.GET)
 	public ResponseEntity<Results> get(){
 		Results rl = serv.run();	
 		return new ResponseEntity<Results>(rl, HttpStatus.OK);
 	}
-	@RequestMapping(value = "/get-all-gender", method=RequestMethod.GET)
+	@RequestMapping(value = "/getAllGender", method=RequestMethod.GET)
 	public ResponseEntity<Iterable<Gender>> getAllGender(){
 		Iterable<Gender> rl = serv.getAllGender();	
 		return new ResponseEntity<Iterable<Gender>>(rl, HttpStatus.OK);
 	}
-	@RequestMapping(value = "/get-all-test", method=RequestMethod.GET)
+	@RequestMapping(value = "/getAllTest", method=RequestMethod.GET)
 	public ResponseEntity<Iterable<Test>> getAllTest(){
 		Iterable<Test> rl = serv.getAllTest();	
 		return new ResponseEntity<Iterable<Test>>(rl, HttpStatus.OK);
 	}
-	@RequestMapping(value = "/get-one-test/{i}", method=RequestMethod.GET)
+	@RequestMapping(value = "/getOneTest/{i}", method=RequestMethod.GET)
 	public ResponseEntity<Test> getOneTest(@PathVariable("i") Long id){
 		return new ResponseEntity<Test>(serv.getOneTest(id),HttpStatus.OK);
 	}	
-	@RequestMapping(value = "/get-one-gender/{i}", method=RequestMethod.GET)
+	@RequestMapping(value = "/getOneGender/{i}", method=RequestMethod.GET)
 	public ResponseEntity<Gender> getOneGender(@PathVariable("i") Long id){
 		return new ResponseEntity<Gender>(serv.getOneGender(id),HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/post-test", method=RequestMethod.POST)
+	@RequestMapping(value = "/postTest", method=RequestMethod.POST)
 	public ResponseEntity<Test> postTest(@RequestBody Test test){
 		test.setId(null);
 		return new ResponseEntity<Test>(serv.save(test),HttpStatus.OK);
 		
 	}
 	
-	@RequestMapping(value = "/post-gender", method=RequestMethod.POST)
+	@RequestMapping(value = "/postGender", method=RequestMethod.POST)
 	public ResponseEntity<Gender> postGender(@RequestBody Gender test){
 		test.setId(null);
 		return new ResponseEntity<Gender>(serv.save(test),HttpStatus.OK);
 		
 	}
 	
-	@RequestMapping(value = "/put-test", method=RequestMethod.PUT)
+	@RequestMapping(value = "/putTest", method=RequestMethod.PUT)
 	public ResponseEntity<Test> putTest(@RequestBody Test test){
 		return new ResponseEntity<Test>(serv.saveOrUpdate(test),HttpStatus.OK);
 		
 	}
 	
-	@RequestMapping(value = "/put-gender", method=RequestMethod.PUT)
+	@RequestMapping(value = "/putGender", method=RequestMethod.PUT)
 	public ResponseEntity<Gender> putGender(@RequestBody Gender test){
 		return new ResponseEntity<Gender>(serv.saveOrUpdate(test),HttpStatus.OK);
 		
 	}
 	
-	@RequestMapping(value = "/delete-test/{id}", method=RequestMethod.DELETE)
+	@RequestMapping(value = "/deleteTest/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Boolean> deleteTest(@PathVariable("id") Long id){
 		return new ResponseEntity<Boolean>(serv.deleteTest(id),HttpStatus.OK);
 		
 	}
 	
-	@RequestMapping(value = "/delete-gender/{id}", method=RequestMethod.DELETE)
+	@RequestMapping(value = "/deleteGender/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Boolean> deleteGender(@PathVariable("id") Long id){
 		return new ResponseEntity<Boolean>(serv.deleteGender(id),HttpStatus.OK);
 		
